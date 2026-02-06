0c9fdb287146b318f86c8574a768f1a33d0c27f2
pkinif <pierrick-kinif@hotmail.be>
2026-02-06 11:16:39 +0100

    adding renv.lock and conflict.R

---
+++ conflict.R
@@ -0,0 +1,17 @@
+# Load necessary libraries
+library(ggplot2)
+library(dplyr)
+
+# Load the mtcars dataset
+data("mtcars")
+
+# Data manipulation: Add a factor column for cylinders
+mtcars <- mtcars %>%
+  mutate(cyl_factor = factor(cyl, levels = c(4, 6, 8), labels = c("4 Cyl", "6 Cyl", "8 Cyl")))
+
+# Summarize the dataset
+summary_table <- mtcars %>%
+  group_by(cyl_factor) %>%
+  summarise(mean_mpg = mean(mpg, na.rm = TRUE))
+
+print(summary_table)
